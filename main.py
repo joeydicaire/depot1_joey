1d5115caa878afb1b52ecb4f69ebb0fb974cd6f4
joey <2259354@cegepoutaouais.qc.ca>
2023-09-26 14:37:10 -0400

    deuxième commit: ajouter un print et ajouter des detail sur une fonction

--- main.py
+++ main.py
@@ -1,2 +1,22 @@
 print("bonjour...")
 print("je continue a mmodifier mon code")
+# porter des variables
+#new
+def calculer_double(nombre):
+    '''
+    calcule le double d'un nombre
+    :param nombre: le nombre entrer
+    :return: le double du nombre
+    '''
+
+    # la porter de la variable nombre est limité
+    # a la fonction ou elle se trouve calculer_double
+    print(nombre2)
+    return nombre*2
+#programme principale
+# la variable nombre2 a une porter globale
+nombre2 = 55
+nombre = 60
+résultat = calculer_double(6)
+
+print("résultat est:", résultat)
